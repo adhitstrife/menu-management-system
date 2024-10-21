a2f738a4414464b3263a7de03acb573f6027b895
adhitstrife <adhitstrife63@gmail.com>
2024-10-21 17:04:09 +0800

    handle delete menu

--- resources/js/components/atoms/menustring.jsx
+++ resources/js/components/atoms/menustring.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
-import { FiPlus } from 'react-icons/fi';
+import { FiPlus, FiTrash } from 'react-icons/fi';
 import { useRecoilState } from 'recoil';
 import { isCreateMenu } from '../../state/isCreateMenu';
 import { parentMenu } from '../../state/parentMenu';
+import { rootMenu } from '../../state/rootMenu';
 
 const MenuString = ({ menu, expandedMenuIds, toggleMenu }) => {
     const [isCreateNewMenu, setIsCreateNewMenu] = useRecoilState(isCreateMenu)
     const [selectedParentMenu, setSelectedParentMenu] = useRecoilState(parentMenu)
+    const [selectedMenu, setSelectedMenu] = useRecoilState(rootMenu);
 
     const addNewMenu = (selectedMenu) => {
         setIsCreateNewMenu(true);
         setSelectedParentMenu(menu);
+    };
+
+    const deleteMenu = (id) => {
+        axios.delete(`/api/menus/${id}`).then(() => {
+            axios.get(`/api/menus/${selectedMenu.id}`).then(response => {
+                setSelectedMenu(response.data);
+            });
+        });
     };
 
     return (
@@ -21,6 +31,9 @@ const MenuString = ({ menu, expandedMenuIds, toggleMenu }) => {
             <span>{menu.name}</span>
             <button onClick={() => addNewMenu(menu)} className="bg-blue-500 hover:bg-blue-600 text-white rounded-full p-0.5 shadow-md focus:outline-none mx-2 invisible group-hover:visible">
                 <FiPlus className="text-xl" />
+            </button>
+            <button onClick={() => deleteMenu(menu.id)} className="bg-red-500 hover:bg-red-600 text-white rounded-full p-0.5 shadow-md focus:outline-none mx-2 invisible group-hover:visible">
+                <FiTrash className="text-xl" />
             </button>
         </div>
     );
