998372a909fc13b460d4c9dbf30873a81df8f38e
adhitstrife <adhitstrife63@gmail.com>
2024-10-21 15:56:53 +0800

    update vite again

--- vite.config.js
+++ vite.config.js
@@ -8,8 +8,11 @@ export default defineConfig({
     },
     build: {
         manifest: true,
-        outDir: "public/build",
-        assetsDir: "",
+        outDir: "public/build", // Outputs files to public/build
+        assetsDir: "", // Ensures files are placed directly in public/build without extra folders
+        rollupOptions: {
+            input: ["resources/css/app.css", "resources/js/app.jsx"],
+        },
     },
     plugins: [
         laravel({
